22932d9298390e5b7741d8aa0b26ba2af180b304
Sahil <shlbnsl843@gmail.com>
2020-06-30 12:48:26 +0530

    Update styles and add mark_as_complete

---
+++ db/migrate/20200630061653_add_imp_times_to_todo_items.rb
@@ -0,0 +1,7 @@
+class AddImpTimesToTodoItems < ActiveRecord::Migration[6.0]
+  def change
+    add_column :todo_items, :completed_at, :datetime
+    add_column :todo_items, :due_at, :datetime
+    add_column :todo_items, :priority, :integer
+  end
+end
